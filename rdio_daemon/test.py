596186dbdac2e9392410db85e05693acbfc05dba
Robert Ely <robertely@gmail.com>
2018-03-04 01:16:23 -0700

    stuff

--- rdio_daemon/test.py
+++ rdio_daemon/test.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python
 from time import sleep
-from lib.audio_player import audio_player
+from lib import audio_player
 # WNYC: http://wnyc-ice.streamguys1.com:80/njprwe
 # Static: RadioStatic.mp3
 
-static = audio_player('../RadioStatic.mp3')
+static = audio_player.audio_player('../RadioStatic.mp3')
 static.setvol(50)
 static.play()
 
-y = audio_player('http://wnyc-ice.streamguys1.com:80/njprwe')
+y = audio_player.audio_player('http://wnyc-ice.streamguys1.com:80/njprwe')
 y.setvol(100)
 y.play()
 
-x = audio_player('http://stream1.cprnetwork.org:80/cpr3_lo')
+x = audio_player.audio_player('http://stream1.cprnetwork.org:80/cpr3_lo')
 x.setvol(0)
 x.play()
 
 sleep(5)
 # From y to static
 for i in range(1,100):
-    audio_player.lin_fade(y,static,i)
+    audio_player.audio_player.lin_fade(y,static,i)
     sleep(.05)
 sleep(2)
 # From static to x
 for i in range(1,100):
-    audio_player.lin_fade(static,x,i)
+    audio_player.audio_player.lin_fade(static,x,i)
     sleep(.05)
 sleep(2)
 # From x to static
 for i in range(1,100):
-    audio_player.lin_fade(x,static,i)
+    audio_player.audio_player.lin_fade(x,static,i)
     sleep(.05)
 sleep(10)
 exit(0)
