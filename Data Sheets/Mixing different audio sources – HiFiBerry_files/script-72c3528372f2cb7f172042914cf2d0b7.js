8e8c0518fe36a6df2206569c4a0f26a73cea3633
Robert Ely <robertely@gmail.com>
2018-03-12 23:02:41 -0600

    toml

---
+++ Data Sheets/Mixing different audio sources – HiFiBerry_files/script-72c3528372f2cb7f172042914cf2d0b7.js
@@ -0,0 +1,76 @@
+/*
+ * jQuery v1.9.1 included
+ */
+
+$(document).ready(function() {
+
+  // social share popups
+  $(".share a").click(function(e) {
+    e.preventDefault();
+    window.open(this.href, "", "height = 500, width = 500");
+  });
+
+  // toggle the share dropdown in communities
+  $(".share-label").on("click", function(e) {
+    e.stopPropagation();
+    var isSelected = this.getAttribute("aria-selected") == "true";
+    this.setAttribute("aria-selected", !isSelected);
+    $(".share-label").not(this).attr("aria-selected", "false");
+  });
+
+  $(document).on("click", function() {
+    $(".share-label").attr("aria-selected", "false");
+  });
+
+  // show form controls when the textarea receives focus or backbutton is used and value exists
+  var $answerbodyTextarea = $(".answer-body textarea"),
+  $answerFormControls = $(".answer-form-controls"),
+  $commentContainerTextarea = $(".comment-container textarea"),
+  $commentContainerFormControls = $(".comment-form-controls");
+
+  $answerbodyTextarea.one("focus", function() {
+    $answerFormControls.show();
+  });
+
+  $commentContainerTextarea.one("focus", function() {
+    $commentContainerFormControls.show();
+  });
+
+  if ($commentContainerTextarea.val() !== "") {
+    $commentContainerFormControls.show();
+  }
+
+  if ($answerbodyTextarea.val() !== "") {
+    $answerFormControls.show();
+  }
+
+  // Submit requests filter form in the request list page
+  $("#request-status-select, #request-organization-select")
+    .on("change", function() {
+      search();
+    });
+
+  // Submit requests filter form in the request list page
+  $("#quick-search").on("keypress", function(e) {
+    if (e.which === 13) {
+      search();
+    }
+  });
+
+  function search() {
+    window.location.search = $.param({
+      query: $("#quick-search").val(),
+      status: $("#request-status-select").val(),
+      organization_id: $("#request-organization-select").val()
+    });
+  }
+
+  // Submit organization form in the request page
+  $("#request-organization select").on("change", function() {
+    this.form.submit();
+  });
+  
+  // Fix for topic display
+  $("article.post").parent().width("100%");
+
+});
