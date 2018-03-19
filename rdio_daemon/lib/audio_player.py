c15f7a87ae8fa923603e73b2124d640cd25e5b98
Robert Ely <robertely@gmail.com>
2018-03-18 21:46:44 -0600

    derps

--- rdio_daemon/lib/audio_player.py
+++ rdio_daemon/lib/audio_player.py
@@ -4,24 +4,30 @@ from math import log
 class audio_player:
     def __init__(self, mediapath = ''):
         self.mediapath = mediapath
-        self.instance = Instance()
+        self.instance = Instance('--input-repeat=-1')
         self.player = self.instance.media_player_new()
         self.media = self.instance.media_new(self.mediapath)
         self.player.set_media(self.media)
+        self.volume = 0
+        self.playing = False
 
     def load(self, mediapath):
-        self.mediapath = mediapath
-        self.media = self.instance.media_new(self.mediapath)
-        self.player.set_media(self.media)
+        if self.mediapath != mediapath:
+            self.mediapath = mediapath
+            self.media = self.instance.media_new(self.mediapath)
+            self.player.set_media(self.media)
 
     def play(self):
-        self.player.play()
+        if !self.playing:
+            self.player.play()
 
     def pause(self):
-        self.player.pause()
+        if self.playing:
+            self.player.pause()
 
     def setvol(self, vol):
-        self.player.audio_set_volume(vol)
+         if vol != self.volume:
+             self.player.audio_set_volume(vol)
 
 # Fade from A to B, a value from 0-100
 def lin_fade(vlc_A, vlc_B, fade):
